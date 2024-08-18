e9671857fe22028f14a4d8ef2eda16261e53dd97
Lloyd <llorugwe@gmail.com>
2024-08-18 10:08:21 -0700

    My commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,18 +5,18 @@
 
 
 def pascal_triangle(n):
-        """
-                Create rows for pascal triangle
-                    """
-                        triangle = []
+    """
+        Create rows for pascal triangle
+    """
+    triangle = []
 
-                            if n <= 0:
-                                        return triangle
+    if n <= 0:
+        return triangle
 
-                                        for i in range(n):
-                                                    row = [1] * (i + 1)
-                                                            for j in range(1, i):
-                                                                            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-                                                                                    triangle.append(row)
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
 
-                                                                                        return triangle
+    return triangle
